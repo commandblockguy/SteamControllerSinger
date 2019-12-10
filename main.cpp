74e74f32499aa5a5971d75d43c4f48532205655e
commandblockguy <commandblockguy1@gmail.com>
2019-12-09 19:24:49 -0600

    first commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <signal.h>
 #include <stdio.h>
 
+#include <fstream>
+
 #include "libusb/libusb.h"
 #include "midifile/midifile.h"
 
@@ -20,12 +22,18 @@
 
 using namespace std;
 
+//adding this here because lazy
+unsigned int note_time;
+unsigned int num_notes = 0;
+ofstream file;
+
 
 struct ParamsStruct{
     const char* midiSong;
     unsigned int intervalUSec;
     int libusbDebugLevel;
     bool repeatSong;
+    bool export_calc;
 };
 
 struct SteamControllerInfos{
@@ -60,11 +68,24 @@ bool SteamController_OpenAndClaim(SteamControllerInfos* controller){
     //On Linux, automatically detach and reattach kernel module
     libusb_set_auto_detach_kernel_driver(controller->dev_handle,1);
 
+    if(libusb_kernel_driver_active(controller->dev_handle, controller->interfaceNum)) {
+        cout<<"Kernel driver active before"<<endl;
+    }
+
+    int err = libusb_detach_kernel_driver(controller->dev_handle, controller->interfaceNum);
+    if(err < 0 && err != LIBUSB_ERROR_NOT_FOUND) {
+        cout<<"Interface detach Error: "<<libusb_strerror(err)<<endl;
+        return false;
+    }
+
+    if(libusb_kernel_driver_active(controller->dev_handle, controller->interfaceNum)) {
+        cout<<"Kernel driver active after"<<endl;
+    }
+
     //Claim the USB interface controlling the haptic actuators
     int r = libusb_claim_interface(controller->dev_handle,controller->interfaceNum);
     if(r < 0) {
-        cout<<"Interface claim Error "<<r<<endl;
-        std::cin.ignore();
+        cout<<"Interface claim Error: "<<libusb_strerror(r)<<endl;
         libusb_close(controller->dev_handle);
         return false;
     }
@@ -75,19 +96,18 @@ bool SteamController_OpenAndClaim(SteamControllerInfos* controller){
 void SteamController_Close(SteamControllerInfos* controller){
     int r = libusb_release_interface(controller->dev_handle,controller->interfaceNum);
     if(r < 0) {
-        cout<<"Interface release Error "<<r<<endl;
-        std::cin.ignore();
+        cout<<"Interface release Error: "<<libusb_strerror(r)<<endl;
         return;
     }
     libusb_close(controller->dev_handle);
 }
 
 
-int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int note,double duration = DURATION_MAX){
+int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int note, double duration, ParamsStruct* params){
     double midiFrequency[128]  = {8.1758, 8.66196, 9.17702, 9.72272, 10.3009, 10.9134, 11.5623, 12.2499, 12.9783, 13.75, 14.5676, 15.4339, 16.3516, 17.3239, 18.354, 19.4454, 20.6017, 21.8268, 23.1247, 24.4997, 25.9565, 27.5, 29.1352, 30.8677, 32.7032, 34.6478, 36.7081, 38.8909, 41.2034, 43.6535, 46.2493, 48.9994, 51.9131, 55, 58.2705, 61.7354, 65.4064, 69.2957, 73.4162, 77.7817, 82.4069, 87.3071, 92.4986, 97.9989, 103.826, 110, 116.541, 123.471, 130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942, 261.626, 277.183, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305, 440, 466.164, 493.883, 523.251, 554.365, 587.33, 622.254, 659.255, 698.456, 739.989, 783.991, 830.609, 880, 932.328, 987.767, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.1, 11175.3, 11839.8, 12543.9};
 
     unsigned char dataBlob[64] = {0x8f,
-                                  0x07,
+                                  0x08, //7
                                   0x00, //Trackpad select : 0x01 = left, 0x00 = right
                                   0xff, //LSB Pulse High Duration
                                   0xff, //MSB Pulse High Duration
@@ -122,10 +142,16 @@ int SteamController_PlayNote(SteamControllerInfos* controller, int haptic, int n
     dataBlob[7] = repeatCount % 0xff;
     dataBlob[8] = repeatCount / 0xff;
 
+    if(params && params->export_calc) {
+        file.write((char*)&note_time, 3);
+        file.write((char*)&dataBlob[2], 7);
+    }
+    num_notes++;
+
     int r;
-    r = libusb_control_transfer(controller->dev_handle,0x21,9,0x0300,2,dataBlob,64,1000);
+    r = libusb_control_transfer(controller->dev_handle,0x21,9,0x0300,controller->interfaceNum,dataBlob,64,1000);
     if(r < 0) {
-        cout<<"Command Error "<<r<< endl;
+        cout<<"Command Error: "<<libusb_strerror(r)<< endl;
         return 1;
     }
 
@@ -168,7 +194,7 @@ void displayPlayedNotes(int channel, int8_t note){
         }
     }
 
-    cout << "\r" ;
+    cout << "\r\n" ;
     cout.flush();
 }
 
@@ -213,6 +239,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 
         //Iterate through all events until the current time, and selecte potential events to play
         for( ; currentEvent != NULL && MidiFileEvent_getTick(currentEvent) < currentTick ; currentEvent = MidiFileEvent_getNextEventInFile(currentEvent)){
+            note_time = MidiFile_getTimeFromTick(midifile, MidiFileEvent_getTick(currentEvent)) * 32768;
 
             //Only process note start events or note end events matching previous event
             if (!MidiFileEvent_isNoteStartEvent(currentEvent) && !MidiFileEvent_isNoteEndEvent(currentEvent)) continue;
@@ -253,7 +280,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
             }
 
             //Play notes
-            SteamController_PlayNote(controller,currentChannel,eventNote);
+            SteamController_PlayNote(controller,currentChannel,eventNote, DURATION_MAX, &params);
             displayPlayedNotes(currentChannel,eventNote);
         }
     }
@@ -267,7 +294,7 @@ void playSong(SteamControllerInfos* controller,const ParamsStruct params){
 
 bool parseArguments(int argc, char** argv, ParamsStruct* params){
     int c;
-    while ( (c = getopt(argc, argv, "l:i:r")) != -1) {
+    while ( (c = getopt(argc, argv, "l:i:r:e")) != -1) {
         unsigned long int value;
 	switch(c){
         case 'l':
@@ -284,6 +311,9 @@ bool parseArguments(int argc, char** argv, ParamsStruct* params){
             break;
         case 'r':
             params->repeatSong = true;
+            break;
+        case 'e':
+            params->export_calc = true;
             break;
         case '?':
             return false;
@@ -304,7 +334,7 @@ bool parseArguments(int argc, char** argv, ParamsStruct* params){
 
 void abortPlaying(int){
     for(int i = 0 ; i < CHANNEL_COUNT ; i++){
-        SteamController_PlayNote(&steamController1,i,NOTE_STOP);
+        SteamController_PlayNote(&steamController1,i,NOTE_STOP, DURATION_MAX, NULL);
     }
 
     SteamController_Close(&steamController1);
@@ -323,6 +353,7 @@ int main(int argc, char** argv)
     params.libusbDebugLevel = LIBUSB_LOG_LEVEL_NONE;
     params.repeatSong = false;
     params.midiSong = "\0";
+    params.export_calc = false;
 
 
     //Parse arguments
@@ -335,8 +366,7 @@ int main(int argc, char** argv)
     //Initializing LIBUSB
     int r = libusb_init(NULL);
     if(r < 0) {
-        cout<<"LIBUSB Init Error "<<r<<endl;
-        std::cin.ignore();
+        cout<<"LIBUSB Init Error: "<<libusb_strerror(r)<<endl;
         return 1;
     }
 
@@ -349,6 +379,9 @@ int main(int argc, char** argv)
 
     //Set mecanism to stop playing when closing process
     signal(SIGINT, abortPlaying);
+    if(params.export_calc) {
+        file.open("data.bin", ios::out | ios::binary);
+    }
 
     //Playing song
     do{
@@ -360,6 +393,12 @@ int main(int argc, char** argv)
     SteamController_Close(&steamController1);
 
     libusb_exit(NULL);
+
+    cout << "Added " << num_notes << " notes." << endl;
+
+    if(params.export_calc) {
+        file.close();
+    }
 
     return 0;
 }
